3910d3c6abca04fb1b67b2384c704d4c84580971
90fabricio09 <90934129+90fabricio09@users.noreply.github.com>
2024-04-08 14:59:41 -0300

    Add files via upload

---
+++ assets/js/window.js
@@ -0,0 +1,21 @@
+document.getElementById('toggleButton').addEventListener('click', function() {
+    var aba = document.getElementById('aba');
+    var icon1 = document.getElementById('icon1');
+    var icon2 = document.getElementById('icon2');
+    if (aba.style.display === 'none') {
+      aba.style.display = 'block';
+      setTimeout(function() {
+        aba.style.opacity = 1;
+      }, 1); // Timeout for 1ms to wait for display to change to block
+      icon1.style.display = 'none';
+      icon2.style.display = 'inline';
+    } else {
+      aba.style.opacity = 0;
+      setTimeout(function() {
+        aba.style.display = 'none';
+      }, 300); // Timeout for 300ms to wait for opacity transition to finish
+      icon1.style.display = 'inline';
+      icon2.style.display = 'none';
+    }
+  });
+  
